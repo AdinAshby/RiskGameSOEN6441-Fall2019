37034b9130651853628e5fc292e44416d7a76105
f_yazdan <f_yazdan@SNELL.ENCS.concordia.ca>
2019-10-07 11:20:55 -0400

    country

--- RiskGameSOEN6441-Fall2019/src/model/Continent.java
+++ RiskGameSOEN6441-Fall2019/src/model/Continent.java
@@ -3,24 +3,31 @@ package model;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Continent {
 	private String name;
-	private int continentId;
+	private static int continentId=1;
+	private int continentControlValue;
 	private static int continentsCounter = 0;
 	private List<Country> countryList = new LinkedList<Country>();
 	private AdjacencyList continentAdjacency = new AdjacencyList();
 	
-	private enum color {
-		RED, BLUE, YELLOW, GREEN
-	};
-
-	public Continent(String name, int continentId) {
+		public Continent(String name, int continentControlValue) {
 		super();
 		this.name = name;
-		this.continentId = continentId;
+		this.continentControlValue= continentControlValue;
+		this.continentId ++;
 		continentAdjacency.addVertex(continentId);
 	}
+
+	public int getContinentControlValue() {
+			return continentControlValue;
+		}
+
+		public void setContinentControlValue(int continentControlValue) {
+			this.continentControlValue = continentControlValue;
+		}
 
 	public List<Country> getCountriesList() {
 		return countryList;
@@ -56,6 +63,47 @@ public class Continent {
 		
 		continentAdjacency.showListEdges();
 	}
+	/**
+	 * 
+	 * @param c as Country
+	 */
+	public void addCountry(Country c) {
+		countryList.add(c);
+	}
+
+
+	public void removeCountry(String countryName) {
+		
+		ListIterator list_Iter = countryList.listIterator();
+		while (list_Iter.hasNext()) {
+
+			Country country = (Country) list_Iter.next();
+
+		if (country.getCountryName().equals(countryName)) {
+			countryList.remove(country);
+			break;
+		}
+		
+	}
+	}
+		
+	public void printCountryList() {
+
+		ListIterator list_Iter = countryList.listIterator();
+		while (list_Iter.hasNext()) {
+
+			Country country = (Country) list_Iter.next();
+
+			System.out.println("Country name is " + country.getCountryName() + ", Country Id is: "
+					+ country.getCountryId());
+			country.showCountryAdjacency();
+		}
+
+		System.out.println("------------------------");
+
+	}
+
+
 }
 
 
--- RiskGameSOEN6441-Fall2019/src/model/Country.java
+++ RiskGameSOEN6441-Fall2019/src/model/Country.java
@@ -65,6 +65,7 @@ public class Country {
 //	  }
 
 	  
+	  
 		public void showCountryAdjacency() {
 			countryAdjacency.showListEdges();
 			
