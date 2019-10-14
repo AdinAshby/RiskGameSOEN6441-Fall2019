ea906064ceec3f4ea264d95ff08748869e372c15
Adin Ashby <adin.ashby@gmail.com>
2019-10-14 11:22:16 -0400

    Added Player Class details.

--- RiskGameSOEN6441-Fall2019/src/model/Player.java
+++ RiskGameSOEN6441-Fall2019/src/model/Player.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Player {
+	
+	private int playerID;
+	private String playerName;
+	private int[] countryID;
+	
+	public Player(int playerID, String playerName, int[] countryID) {
+		this.playerID = playerID;
+		this.playerName = playerName;
+		this.countryID = countryID;
+	}
+	
+	public int getPlayerID() {
+		return playerID;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public int[] getCountryID() {
+		return countryID;
+	}
+
+	public void setCountryID(int[] countriesIds) {
+		this.countryID = countriesIds;
+	}
 
 }
