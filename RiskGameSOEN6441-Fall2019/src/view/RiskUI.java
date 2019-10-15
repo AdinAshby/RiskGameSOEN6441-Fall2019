893bfb6c40678eb89b234a8735f59f661aa75c1c
Adin Ashby <adin.ashby@gmail.com>
2019-10-14 20:33:16 -0400

    Did some work on RiskUI.

--- RiskGameSOEN6441-Fall2019/src/view/RiskUI.java
+++ RiskGameSOEN6441-Fall2019/src/view/RiskUI.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.util.Scanner;
+
 public class RiskUI {
 	private String welcomeMessage = "\t\t*****Risk Game*****";
 	private String editMapYesOrNoMessage = "Do you want to create/edit map? (Y/N)\n";
@@ -11,4 +13,162 @@ public class RiskUI {
 	private String reinforceRequestingMessage = "Now, it's time to reinforce!\n";
 	private String fortifyRequestingMessage = "Let's fortify! or type in \"fortify none\" if you don't want to.\n";
 
+	private Scanner scanner;
+
+	public RiskUI() {
+		scanner = new Scanner(System.in);
+	}
+
+	public void RiskUIStartTheGame() {
+
+		String editMapYesOrNoAnswer;
+		boolean editMapWhileStatus = true;
+
+		String loadMapCommands;
+		String loadMapFilePath;
+		boolean loadMapWhileStatus = true;
+
+		String playersCommands;
+		String addOrRemovePlayerCommand;
+		String playerName;
+		boolean playersWhileStatus = true;
+
+		String startupCommands;
+		String countryNamePlaceArmyCommand;
+		int numberPlaceArmyCommand;
+		boolean startupWhileStatus = true;
+		
+
+		while(editMapWhileStatus) {
+
+			System.out.println(welcomeMessage);
+			System.out.println(editMapRequestingMessage);
+			editMapYesOrNoAnswer = scanner.next();
+
+			switch (editMapYesOrNoAnswer) {
+			case "Y":
+			case "y":
+
+
+				editMapWhileStatus = false;
+				break;
+
+			case "N":
+			case "n":
+
+				editMapWhileStatus = false;
+				break;
+
+			default:
+				System.out.println("Your answer is not valid! Try again...\n");
+				break;
+			}
+		}
+
+		System.out.println(loadMapRequestingMessage);
+		loadMapCommands = scanner.next();
+
+
+		while(loadMapWhileStatus) {
+			switch (loadMapCommands) {
+
+			case "loadmap":
+			case "LOADMAP":
+				loadMapFilePath = scanner.next();
+				loadMapWhileStatus = false;
+				break;
+
+			default:
+				System.out.println("Your command is not valid! Try again...\\n");
+				break;
+			}
+		}
+
+		System.out.println(addOrRemovePlayersRequestingMessage);
+
+		while(playersWhileStatus) {
+			playersCommands = scanner.next();
+
+			switch (playersCommands) {
+			case "gameplayer":
+			case "GAMEPLAYER":
+				if (scanner.hasNext()) 
+					while (scanner.hasNext()) {
+						addOrRemovePlayerCommand = scanner.next();
+						playerName = scanner.next();
+
+						switch (addOrRemovePlayerCommand) {
+						case "-add":
+						case "-ADD":
+
+							break;
+
+						case "-remove":
+						case "-REMOVE":
+
+							break;
+						}
+					}
+				else 
+					System.out.println("Your command is not valid! Try again...\\n");
+
+				break;
+
+			case "populatecountries":
+			case "POPULATECOUNTRIES":
+
+				playersWhileStatus = false;
+				break;
+
+			default:
+				System.out.println("Your command is not valid! Try again...\\n");
+				break;
+			}
+		}
+
+		System.out.println(startupRequestingMessage);
+
+		for(int i = 0; i < ...; i++) {
+
+			System.out.println("Player" + ...);
+			startupWhileStatus = true;
+
+			while(startupWhileStatus) {
+				System.out.println("You have -" + ... + "- army(ies) to place!");
+				startupCommands = scanner.next();
+
+				switch (startupCommands) {
+				case "placearmy":
+				case "PLACEARMY":
+					if (scanner.hasNext()) 
+						while (scanner.hasNext()) {
+							countryNamePlaceArmyCommand = scanner.next();
+							numberPlaceArmyCommand = scanner.nextInt();
+							
+						}
+					else 
+						System.out.println("Your command is not valid! Try again...\\n");
+
+
+					break;
+
+				case "placeall":
+				case "PLACEALL":
+
+					startupWhileStatus = false;
+					break;
+
+				default:
+					System.out.println("Your command is not valid! Try again...\\n");
+					break;
+				}	
+			}
+		}
+		
+		System.out.println(reinforceRequestingMessage);
+		
+		
+		System.out.println(fortifyRequestingMessage);
+		
+	}
 }
