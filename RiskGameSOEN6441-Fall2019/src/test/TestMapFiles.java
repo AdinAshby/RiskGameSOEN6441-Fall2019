45fef4d1b5b6ff3b967201f7d03da3679e21f798
s_shehna <s_shehna@HUB.ENCS.concordia.ca>
2019-10-15 22:13:12 -0400

    map files tested

--- RiskGameSOEN6441-Fall2019/src/test/TestMapFiles.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestMapFiles.java
@@ -1,14 +1,19 @@
 package test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class TestMapFiles {
-
+import org.junit.Test;
+import org.junit.Assert;
+import model.MapBuilder;
+public class TestMapFiles {
+	MapBuilder mb = new MapBuilder();
+	   String valid_file = "test";
+	   String invalid_file = "test2";
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testValidMapFiles() throws Exception {
+		Assert.assertEquals(true, mb.loadMap(valid_file));
+	}
+	@Test
+	public void testInvalidMapFiles() throws Exception
+	{
+		Assert.assertEquals(false, mb.loadMap(invalid_file));
 	}
 
 }
