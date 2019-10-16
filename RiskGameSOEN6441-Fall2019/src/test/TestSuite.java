dab730ab60ef9c6e92ae59e60dcabbd3438991cb
s_shehna <s_shehna@TUXEDO.ENCS.concordia.ca>
2019-10-16 11:42:00 -0400

    documentation updated

--- RiskGameSOEN6441-Fall2019/src/test/TestSuite.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestSuite.java
@@ -3,7 +3,11 @@ package test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+/**
+ * This test suite contain all the test classes
+ * @author s_shehna
+ *
+ */
 @RunWith(Suite.class)
 @SuiteClasses({ TestMap.class, TestMapFiles.class, TestPlayer.class })
 public class TestSuite {
