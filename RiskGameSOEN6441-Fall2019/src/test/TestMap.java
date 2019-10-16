4427af5775b8f6c7d90649c546287452526f4c9f
s_shehna <s_shehna@TUXEDO.ENCS.concordia.ca>
2019-10-16 10:35:06 -0400

    testing maps

--- RiskGameSOEN6441-Fall2019/src/test/TestMap.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestMap.java
@@ -1,15 +1,37 @@
 package test;
 
-import org.junit.jupiter.api.Test;
-import org.junit.Assert;
-import model.MapBuilder;
+import  org.junit.Assert;
+
+import org.junit.Test;
 import model.AdjacencyList;
+import model.MapBuilder;
 public class TestMap {
-	MapBuilder mb = new MapBuilder();
-	AdjacencyList ad = new AdjacencyList();
+MapBuilder mb = new MapBuilder();
+AdjacencyList ad = new AdjacencyList();
+String valid_map = "test";
+String invalid_map = "test6";
 	@Test
-	void testValidateMap() 
+	public void testvalidateMap() throws Exception
 	{
-		Assert.assertEquals(true, ad.isConnected());
+		Assert.assertEquals(true, mb.loadMap(valid_map));
+		Assert.assertEquals(true, mb.validateMap());
+		Assert.assertEquals(false, mb.loadMap(invalid_map));
+		Assert.assertEquals(false, mb.validateMap());
 	}
+	@Test
+	public void testConnectedGraph() throws Exception
+	{
+		Assert.assertEquals(true, mb.loadMap(valid_map));
+		Assert.assertEquals(true, ad.isConnected() );
+		Assert.assertEquals(false, mb.loadMap(invalid_map));
+		Assert.assertEquals(false, ad.isConnected() );
+	}
+@Test
+public void testisMapSubGraph() throws Exception
+{
+	Assert.assertEquals(true, mb.loadMap(valid_map));
+	Assert.assertEquals(true, mb.isMapSubGraph());
+	Assert.assertEquals(false, mb.loadMap(invalid_map));
+	Assert.assertEquals(false, mb.isMapSubGraph());
+}
 }
