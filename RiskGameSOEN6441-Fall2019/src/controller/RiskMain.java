ec82aca37c2fd6f0db12355e09e65e1b1ef1bd5d
Golnoosh72 <yazdani7@yahoo.com>
2019-10-17 11:39:11 -0400

    document

--- RiskGameSOEN6441-Fall2019/src/controller/RiskMain.java
+++ RiskGameSOEN6441-Fall2019/src/controller/RiskMain.java
@@ -2,10 +2,20 @@ package controller;
 
 import view.RiskUI;
 
+/**
+ * This is a Risk Main Driver
+ * 
+ * @author f_yazdan
+ *
+ */
 public class RiskMain {
-
+	/**
+	 * Create an object of the UI risk to start the game
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		RiskUI rui=new RiskUI();
+		RiskUI rui = new RiskUI();
 		rui.RiskUIStartTheGame();
 
 	}
