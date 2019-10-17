8b67a9445904cffd5179f54dbe4310d90a791197
s_shehna <s_shehna@CRAWFORD.ENCS.concordia.ca>
2019-10-17 16:21:46 -0400

    test classes updated

---
+++ RiskGameSOEN6441-Fall2019/src/test/TestReinforcement.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+import model.MapBuilder;
+
+import model.Player;
+/**
+ * This testcase class test the reinforcement phase
+ * @s_shehna
+ */
+public class TestReinforcement {
+ int countryID[] = {1,2,3,4,5,6,7,8,9};
+	Player player1 = new Player("shehnaz", countryID);
+	MapBuilder mb = new MapBuilder();
+	@Test
+	public void testArmies() {
+	 
+	}
+
+}
