2fd47447e1b8bd4b5e5b43b640f2611150368031
Golnoosh72 <yazdani7@yahoo.com>
2019-10-17 11:00:42 -0400

    document

--- RiskGameSOEN6441-Fall2019/src/model/Continent.java
+++ RiskGameSOEN6441-Fall2019/src/model/Continent.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+
 /**
  * This class is made for defining continents
  */
@@ -13,51 +14,93 @@ public class Continent {
 	 */
 	private String name;
 	/**
-	 * private continentId 
+	 * private continent Id
 	 */
-	private int continentId=1;
+	private int continentId = 1;
+	/**
+	 * private continent Control Value
+	 */
 	private int continentControlValue;
+	/**
+	 * private static continents Counter
+	 */
 	private static int continentsCounter = 0;
+	/**
+	 * private countryList_list of the countries
+	 */
 	private List<Country> countryList = new LinkedList<Country>();
 
-
+	/**
+	 * 
+	 * @return continents Counter
+	 */
 	public static int getContinentsCounter() {
 		return continentsCounter;
 	}
+
+	/**
+	 * set continents Counter
+	 * 
+	 * @param continentsCounter
+	 */
 
 	public static void setContinentsCounter(int continentsCounter) {
 		Continent.continentsCounter = continentsCounter;
 	}
 
+	/**
+	 * parameterized constructor
+	 * 
+	 * @param name
+	 * @param continentControlValue
+	 */
 	public Continent(String name, int continentControlValue) {
 		super();
 		this.name = name;
-		this.continentControlValue= continentControlValue;
+		this.continentControlValue = continentControlValue;
 		continentsCounter++;
-		this.continentId =continentsCounter;
+		this.continentId = continentsCounter;
 
-		//		continentAdjacency.addVertex(continentId);
+		// continentAdjacency.addVertex(continentId);
 	}
-
+/**
+ * it return the continent Control Value
+ * @return continentControlValue
+ */
 	public int getContinentControlValue() {
 		return continentControlValue;
 	}
+	/**
+	 * it set the continent Control Value
+	 * @param continentControlValue
+	 */
 
 	public void setContinentControlValue(int continentControlValue) {
 		this.continentControlValue = continentControlValue;
 	}
-
+/**
+ * it return the list of the countries
+ * @return countryList
+ */
 	public List<Country> getCountriesList() {
 		return countryList;
 	}
+	/**
+	 * set country list
+	 * @param countriesList
+	 */
 
 	public void setCountriesList(LinkedList<Country> countriesList) {
 		this.countryList = countriesList;
 	}
-
+/**
+ * it return name of the continent
+ * @return name
+ */
 	public String getContinentName() {
 		return name;
 	}
+
 	/**
 	 * 
 	 * @param name
@@ -65,24 +108,38 @@ public class Continent {
 	public void setContinentName(String name) {
 		this.name = name;
 	}
-
+/**
+ * 
+ * @return continentId
+ */
 	public int getContinentId() {
 		return this.continentId;
 	}
-
+/**
+ * 
+ * @param continentId
+ */
 	public void setContinentId(int continentId) {
 		this.continentId = continentId;
 	}
-
+/**
+ * 
+ * @param countryId
+ * @return country if there is any otherwise return false
+ */
 	public Country getCountry(int countryId) {
 		for (Country country : countryList) {
-			if (country.getCountryId()==countryId) {
+			if (country.getCountryId() == countryId) {
 				return country;
 			}
 		}
 		return null;
 	}
-
+/**
+ * 
+ * @param countryName
+ * @return country if the name of the country we pass is the same as country name otherwise return null
+ */
 	public Country getCountry(String countryName) {
 		for (Country country : countryList) {
 			if (country.getCountryName().equals(countryName)) {
@@ -94,7 +151,9 @@ public class Continent {
 
 	/**
 	 * This methods adds a new country in the exisiting countery list
-	 * @param c as Country
+	 * 
+	 * @param c
+	 *            as Country
 	 */
 	public void addCountry(Country c) {
 		countryList.add(c);
@@ -102,7 +161,9 @@ public class Continent {
 
 	/**
 	 * This method removes an existing country
-	 * @param countryName name of country
+	 * 
+	 * @param countryName
+	 *            name of country
 	 * @Type String
 	 */
 	public void removeCountry(String countryName) {
@@ -119,9 +180,10 @@ public class Continent {
 
 		}
 	}
+
 	/**
 	 * This method displays the list of countries
-	 */	
+	 */
 	public void printCountryList() {
 
 		ListIterator list_Iter = countryList.listIterator();
@@ -129,18 +191,21 @@ public class Continent {
 
 			Country country = (Country) list_Iter.next();
 
-			System.out.println("   Country name is " + country.getCountryName() + ", Country Id is: "
-					+ country.getCountryId());
+			System.out.println(
+					"   Country name is " + country.getCountryName() + ", Country Id is: " + country.getCountryId());
 
 		}
 
 		System.out.println("------------------------");
 
 	}
-
+/**
+ * it return list containing the Countries names
+ * @return countries
+ */
 	public ArrayList<String> getCountryListNames() {
 
-		ArrayList<String> countries=new ArrayList<String>();
+		ArrayList<String> countries = new ArrayList<String>();
 
 		ListIterator list_Iter = countryList.listIterator();
 		while (list_Iter.hasNext()) {
@@ -148,15 +213,10 @@ public class Continent {
 			Country country = (Country) list_Iter.next();
 			countries.add(country.getCountryName());
 
-
 		}
 
 		return countries;
 
 	}
 
-
 }
-
-
-
