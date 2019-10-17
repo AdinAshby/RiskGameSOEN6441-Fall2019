b318f280540b4073729b96bad00b2e3e3aa67949
Golnoosh Yazdani <41442666+golnoosh72@users.noreply.github.com>
2019-10-16 20:43:13 -0400

    Update RiskMain.java
    main modified

--- RiskGameSOEN6441-Fall2019/src/controller/RiskMain.java
+++ RiskGameSOEN6441-Fall2019/src/controller/RiskMain.java
@@ -1,9 +1,12 @@
-package main;
+package controller;
+
+import view.RiskUI;
 
 public class RiskMain {
 
 	public static void main(String[] args) {
-		
+		RiskUI rui=new RiskUI();
+		rui.RiskUIStartTheGame();
 
 	}
 
