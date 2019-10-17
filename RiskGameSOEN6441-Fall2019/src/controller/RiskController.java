c4628e12ce37ae8f96f7247d18e3d37bdade2f34
Golnoosh72 <yazdani7@yahoo.com>
2019-10-17 11:37:33 -0400

    document

--- RiskGameSOEN6441-Fall2019/src/controller/RiskController.java
+++ RiskGameSOEN6441-Fall2019/src/controller/RiskController.java
@@ -3,16 +3,38 @@ package controller;
 import model.Player;
 import view.RiskUI;
 
+/**
+ * This is a risk controller class
+ * 
+ * @author f_yazdan
+ *
+ */
 public class RiskController {
 
+	/**
+	 * static instance of the RiskController
+	 */
 	private static RiskController instance;
+	/**
+	 * private riskUI
+	 */
 	private RiskUI riskUI;
+	/**
+	 * private players
+	 */
 	private Player[] players;
-	
+
+	/**
+	 * private RiskController
+	 */
 	private RiskController() {
-		
+
 	}
-	
+
+	/**
+	 * 
+	 * @return instance
+	 */
 	public static RiskController getInstance() {
 		if (instance == null) {
 			instance = new RiskController();
@@ -21,13 +43,13 @@ public class RiskController {
 			return instance;
 		}
 	}
-	
+
+	/**
+	 * 
+	 * @return players
+	 */
 	public Player[] getPlayers() {
 		return this.players;
 	}
-	
-	
-	
-	
-	
+
 }
