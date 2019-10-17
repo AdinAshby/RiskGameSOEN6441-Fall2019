c0f1ff18505b31568163ec7eb0006f2d3581c539
Golnoosh72 <yazdani7@yahoo.com>
2019-10-17 11:47:13 -0400

    document

--- RiskGameSOEN6441-Fall2019/src/test/TestMapFiles.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestMapFiles.java
@@ -1,23 +1,36 @@
 package test;
+
 import org.junit.Test;
 import org.junit.Assert;
 import model.MapBuilder;
+
 /**
  * This class contain testcases to test map files
+ * 
  * @author s_shehna
  *
  */
 public class TestMapFiles {
+	/**
+	 * Object of the MapBuilder
+	 */
 	MapBuilder mb = new MapBuilder();
-	   String valid_file = "test";
-	   String invalid_file = "test2";
+	/**
+	 * String valid_file
+	 */
+	String valid_file = "test";
+	/**
+	 * String inValid_file
+	 */
+	String invalid_file = "test2";
+
 	@Test
 	public void testValidMapFiles() throws Exception {
 		Assert.assertEquals(true, mb.loadMap(valid_file));
 	}
+
 	@Test
-	public void testInvalidMapFiles() throws Exception
-	{
+	public void testInvalidMapFiles() throws Exception {
 		Assert.assertEquals(false, mb.loadMap(invalid_file));
 	}
 
