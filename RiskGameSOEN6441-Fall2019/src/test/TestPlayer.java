930cb9532e70ce783411d66c418563e751fc4352
Golnoosh72 <yazdani7@yahoo.com>
2019-10-17 11:48:48 -0400

    document

--- RiskGameSOEN6441-Fall2019/src/test/TestPlayer.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestPlayer.java
@@ -1,28 +1,39 @@
 package test;
+
 import org.junit.Test;
 import org.junit.Assert;
 import model.Player;
+
 /**
  * This test class tests the player class methods
+ * 
  * @author s_shehna
  *
  */
 public class TestPlayer {
-	int CountryId[] = {1,2,3,4};
-	int CountryId_incorrect[] = {1,2,3,4,35};
-    Player player1 = new Player("shehnaz", CountryId);
+	/**
+	 * int CountryId
+	 */
+	int CountryId[] = { 1, 2, 3, 4 };
+	/**
+	 * int CountryId_incorrect with incorrect initialization
+	 */
+	int CountryId_incorrect[] = { 1, 2, 3, 4, 35 };
+	/**
+	 * object of the player
+	 */
+	Player player1 = new Player("shehnaz", CountryId);
 
 	@Test
-	public void testgetPlayerName()
-	{
+	public void testgetPlayerName() {
 		Assert.assertEquals("shehnaz", player1.getPlayerName());
 		Assert.assertNotEquals("golnoosh", player1.getPlayerName());
 	}
+
 	@Test
-	public void testgetCountryId()
-	{
+	public void testgetCountryId() {
 		Assert.assertArrayEquals("country Ids", CountryId, player1.getCountryIDs());
-		Assert.assertNotSame("Country Ids not same",CountryId_incorrect , player1.getCountryIDs());
+		Assert.assertNotSame("Country Ids not same", CountryId_incorrect, player1.getCountryIDs());
 	}
 
 }
