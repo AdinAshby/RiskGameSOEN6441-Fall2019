6585191907ced608699546380a12ee3cd5b6870c
Golnoosh72 <yazdani7@yahoo.com>
2019-11-05 21:35:00 -0500

    Player changes

--- RiskGameSOEN6441-Fall2019/src/model/Player.java
+++ RiskGameSOEN6441-Fall2019/src/model/Player.java
@@ -1,52 +1,53 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * 
  * This is Player class, in which we define our attributes of a Player.
  * 
- * @author 
- *  
+ * @author
+ * 
  */
 public class Player {
-	
+
 	/**
 	 * PlayerName is name of the player corresponding to the ID.
 	 */
 	private String playerName;
-	
+
 	/**
 	 * List of all countryIDs that the player owns.
 	 */
 	private int[] countryIDs;
-	
+
 	private double percentageControlled = 0.00;
 	private String[] continentsControlled;
 	private int totalNumberOfArmies = 0;
 	private ArrayList<Card> cards;
 	private int playerCountForCard;
 
-	
 	private boolean allowToGetCard;
-	
+
 	/**
-	 * This is  Player constructor method for initializing PlayerName and countryId
+	 * This is Player constructor method for initializing PlayerName and countryId
 	 * 
-	 * @param playerID A unique ID for each player.
+	 * @param playerID   A unique ID for each player.
 	 * @param playerName Corresponding player name.
-	 * @param countryID List of all countries (IDs) that the player owns.
+	 * @param countryID  List of all countries (IDs) that the player owns.
 	 */
 	public Player(String playerName, int[] countryID) {
 		this.playerName = playerName;
 		this.countryIDs = countryID;
 		cards = new ArrayList<Card>();
-		playerCountForCard=0;
+		playerCountForCard = 0;
 	}
-	public void addOneToCardCounter()
-	{
-		 playerCountForCard++;
+
+	public void addOneToCardCounter() {
+		playerCountForCard++;
 	}
+
 	/**
 	 * This is Getter method for PlayerName.
 	 * 
@@ -55,19 +56,18 @@ public class Player {
 	public String getPlayerName() {
 		return playerName;
 	}
+
 	public int getplayerCountForCard() {
 
 		return playerCountForCard;
 
 	}
-	
+
 	public ArrayList<Card> getCards() {
 
 		return cards;
 
 	}
-
-
 
 	public void setCards(ArrayList<Card> cardList) {
 
@@ -85,19 +85,13 @@ public class Player {
 
 	}
 
-
-
 	public void addCard(Card cardToAdd) {
 
 		this.cards.add(cardToAdd);
 
 	}
 
-
-
 	public boolean isMoreThanFive() {
-
-
 
 		if (cards.size() >= 5)
 
@@ -108,13 +102,12 @@ public class Player {
 			return false;
 
 	}
+
 	public boolean getAllowingCardStatus() {
 
 		return this.allowToGetCard;
 
 	}
-
-
 
 	public void setAllowingStatus(boolean status) {
 
@@ -139,28 +132,90 @@ public class Player {
 	public void setCountryIDs(int[] countriesIDs) {
 		this.countryIDs = countriesIDs;
 	}
-	
+
 	public double getPercentageControlled() {
 		return percentageControlled;
 	}
-	
+
 	public void setPercentageControlled(double percentageControlled) {
 		this.percentageControlled = percentageControlled;
 	}
-	
+
 	public String[] getContinentsControlled() {
 		return continentsControlled;
 	}
-	
+
 	public void setContinentsControlled(String[] continentsControlled) {
 		this.continentsControlled = continentsControlled;
 	}
-	
+
 	public int getTotalNumberOfArmies() {
 		return totalNumberOfArmies;
 	}
-	
+
 	public void setTotalNumberOfArmies(int totalNumberOfArmies) {
 		this.totalNumberOfArmies = totalNumberOfArmies;
 	}
+
+	public boolean isAttackValid(MapBuilder mapBuild, int attackerNumDice, Country attackerCountry,
+			Country attackingCountry, boolean enablePrint) {
+		boolean isValid = true;
+		if (attackerNumDice > attackingCountry.getArmies()) {
+			if(enablePrint)
+				System.out.println(
+					"attacking dice ("+attackerNumDice+") should not be more than the number of armies contained in the attacking country");
+			isValid = false;
+		}else if (attackerNumDice > 3) {
+			if(enablePrint)
+				System.out.println(
+						"attacking dice ("+attackerNumDice+") should not be more than 3");
+				isValid = false;
+		} else if (!mapBuild.isAdjacentCountry(attackerCountry.getCountryId(), attackingCountry.getCountryId())) {
+			if(enablePrint)
+				System.out.println("Countries are not adjacent");
+			isValid = false;
+
+			/// Country should be owned and attacking country for opponent
+		}
+		return isValid;
+	}
+
+	public boolean isAttackPossible(MapBuilder mapBuild) {
+		boolean isAttackPossible = false;
+		for (int countyId : countryIDs) {
+			// Country country=mapBuild.getCountryById(countyId);
+//			System.out.println("Checking " + countyId);
+			ArrayList<Integer> CountryAdjList = mapBuild.getCountryAdjacency().getVertexAdjacency(countyId);
+//	System.out.println(CountryAdjList); //Arrays.toString(
+
+			for (int adjCountry : CountryAdjList) {
+				if (!contains(countryIDs, adjCountry)) {
+					for(int i=1;i<4;i++) {
+					if (isAttackValid(mapBuild, i, mapBuild.getCountryById(countyId),
+							mapBuild.getCountryById(adjCountry), false)) {
+						isAttackPossible = true;
+					}
+					}
+				}
+			}
+
+		}
+
+		return isAttackPossible;
+	}
+
+	public static boolean contains(final int[] array, final int v) {
+
+		boolean result = false;
+
+		for (int i : array) {
+			if (i == v) {
+				result = true;
+				break;
+			}
+		}
+
+		return result;
+	}
+
 }
