3d4db6751cd88c4239461fc0ea86c31436d985f8
s_shehna <s_shehna@LIB-W445-020.concordia.ca>
2019-11-06 18:21:19 -0500

    javadoc updated

--- RiskGameSOEN6441-Fall2019/src/test/TestArmies.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestArmies.java
@@ -5,12 +5,19 @@ import model.*;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+/**
+ * This testcase class test the calculation of armies given to players
+ * @author s_shehna
+ *
+ */
 public class TestArmies {
 
 	ArrayList<String> players = new ArrayList<String>();
 	MapBuilder mapBuild = new MapBuilder();
-
+/**
+ * This testcase tests the number of armies given to only player in game
+ *
+ */
 	@Test
 	public void testArmiesOwnedByPlayer1() throws Exception {
 		mapBuild.loadMap("ameroki");
@@ -21,6 +28,10 @@ public class TestArmies {
 	
 		Assert.assertEquals(14, armies_owned_by_player1);	 
 }
+	/**
+	 * This testcase tests the number of armies given to two players in game
+	 * 
+	 */
 	@Test
 	public void testArmiesOwnedByPlayer2() throws Exception {
 		mapBuild.loadMap("ameroki");
@@ -35,6 +46,10 @@ public class TestArmies {
 		Assert.assertEquals(7, armies_owned_by_player1);	
 		Assert.assertEquals(7, armies_owned_by_player2);	
 	}
+	/**
+	 * This testcase tests the calculation for number of armies when there are 3 players
+	 * 
+	 */
 	@Test
 	public void testArmiesOwnedByPlayer3() throws Exception {
 		mapBuild.loadMap("ameroki");
@@ -52,6 +67,10 @@ public class TestArmies {
 		Assert.assertEquals(4, armies_owned_by_player2);	
 		Assert.assertEquals(4, armies_owned_by_player3);	
 	}
+	/**
+	 * This testcase tests the calculation for number of armies when there are 4 players
+	 * 
+	 */
 	@Test
 	public void testArmiesOwnedByPlayer4() throws Exception {
 		mapBuild.loadMap("ameroki");
