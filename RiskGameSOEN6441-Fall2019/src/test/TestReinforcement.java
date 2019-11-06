47e4f017189f69981b1c67db1a03840db5f33b35
s_shehna <s_shehna@HUB.ENCS.concordia.ca>
2019-11-06 12:28:40 -0500

    Reinforcement phase tested

--- RiskGameSOEN6441-Fall2019/src/test/TestReinforcement.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestReinforcement.java
@@ -14,21 +14,23 @@ import model.Player;
  * @s_shehna
  */
 public class TestReinforcement {
-	//int countryID[] = {1,2,3,4,5,6,7,8,9};
-	//Player player1 = new Player("shehnaz", countryID);
+	
 	ArrayList<String> players = new ArrayList<String>();
 	MapBuilder mapBuild = new MapBuilder();
 
 	@Test
-	public void testArmies() throws Exception {
+	public void testReinforceArmiesValid() throws Exception {
 		mapBuild.loadMap("ameroki");
 		players.add("Shehnaz");
-		//players.add("Adin");
 		mapBuild.assigningPlayersToCountries(players);
-		mapBuild.calculateNumberOfArmiesEachPlayerGets("shehnaz");
-		int armies_owned_by_player1 = mapBuild.getNumberOfArmiesEachPlayerGets();
-		System.out.print(armies_owned_by_player1);
-		Assert.assertEquals(14, armies_owned_by_player1);	 
+		Assert.assertEquals(true, mapBuild.reinforceIsValid("shehnaz","india", 12));	 
 	}
-
+	@Test
+	public void testReinforceArmiesInvalid() throws Exception {
+		mapBuild.loadMap("ameroki");
+		players.add("Shehnaz");
+		players.add("golnoosh");
+		mapBuild.assigningPlayersToCountries(players);
+		Assert.assertEquals(true, mapBuild.reinforceIsValid("shehnaz","india", 19));	 
+	}
 }
