0ab975ca7dda794c062ca0b8d3b18a6282f3ee22
s_shehna <s_shehna@HUB.ENCS.concordia.ca>
2019-11-06 11:58:46 -0500

    updated

--- RiskGameSOEN6441-Fall2019/src/test/TestSuite.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestMap.class, TestMapFiles.class, TestPlayer.class, TestReinforcement.class })
+@SuiteClasses({ TestMap.class, TestMapFiles.class, TestPlayer.class, TestReinforcement.class, TestArmies.class })
 public class TestSuite {
 
 }
