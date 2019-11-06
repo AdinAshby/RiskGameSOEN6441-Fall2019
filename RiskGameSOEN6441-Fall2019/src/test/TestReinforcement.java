1b17d1265856e44f7a9dab0d12f4ec3c79104968
s_shehna <s_shehna@LIB-W445-020.concordia.ca>
2019-11-06 18:35:45 -0500

    reinforcement phase tested

--- RiskGameSOEN6441-Fall2019/src/test/TestReinforcement.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestReinforcement.java
@@ -18,34 +18,46 @@ public class TestReinforcement {
 	
 	ArrayList<String> players = new ArrayList<String>();
 	MapBuilder mapBuild = new MapBuilder();
-
+/**
+ * This testcase tests valid number of armies owned by players for reinforcement 
+ *
+ */
 	@Test
 	public void testReinforceArmiesValid() throws Exception {
 		mapBuild.loadMap("ameroki");
 		players.add("Shehnaz");
+		players.add("Golnoosh");
 		mapBuild.assigningPlayersToCountries(players);
 		Player[] myPlayers = mapBuild.getPlayers();
 		
 		Random random = new Random();
 		
-		int randomID = random.nextInt(myPlayers[0].getCountryIDs().length);
-		String countryName = mapBuild.getCountryNameById(myPlayers[0].getCountryIDs()[randomID]);
-		
-		Assert.assertEquals(true, mapBuild.reinforceIsValid("shehnaz", countryName, 12));	 
+		int RandomID_Player1 = random.nextInt(myPlayers[0].getCountryIDs().length);
+		int RandomID_Player2 = random.nextInt(myPlayers[1].getCountryIDs().length);
+		String Country_Owned_By_Player1 = mapBuild.getCountryNameById(myPlayers[0].getCountryIDs()[RandomID_Player1]);
+		String Country_Owned_By_Player2 = mapBuild.getCountryNameById(myPlayers[1].getCountryIDs()[RandomID_Player2]);
+		Assert.assertEquals(true, mapBuild.reinforceIsValid("shehnaz", Country_Owned_By_Player1, 6));
+		Assert.assertEquals(true, mapBuild.reinforceIsValid("Golnoosh", Country_Owned_By_Player2, 6));	 
 	}
+	/**
+	 * This testcase tests the reinforcement method for invalid number of armies owned by players
+	 * @throws Exception
+	 */
 	@Test
 	public void testReinforceArmiesInvalid() throws Exception {
 		mapBuild.loadMap("ameroki");
 		players.add("Shehnaz");
-		players.add("golnoosh");
+		players.add("Golnoosh");
 		mapBuild.assigningPlayersToCountries(players);
 		Player[] myPlayers = mapBuild.getPlayers();
 		
 		Random random = new Random();
-		
-		int randomID = random.nextInt(myPlayers[0].getCountryIDs().length);
-		String countryName = mapBuild.getCountryNameById(myPlayers[0].getCountryIDs()[randomID]);
-		
-		Assert.assertEquals(true, mapBuild.reinforceIsValid("shehnaz", countryName, 40));	 
+
+		int RandomID_Player1 = random.nextInt(myPlayers[0].getCountryIDs().length);
+		int RandomID_Player2 = random.nextInt(myPlayers[1].getCountryIDs().length);
+		String Country_Owned_By_Player1 = mapBuild.getCountryNameById(myPlayers[0].getCountryIDs()[RandomID_Player1]);
+		String Country_Owned_By_Player2 = mapBuild.getCountryNameById(myPlayers[1].getCountryIDs()[RandomID_Player2]);
+		Assert.assertEquals(false, mapBuild.reinforceIsValid("shehnaz", Country_Owned_By_Player1, 40));
+		Assert.assertEquals(false, mapBuild.reinforceIsValid("Golnoosh", Country_Owned_By_Player2, 20));
 	}
 }
