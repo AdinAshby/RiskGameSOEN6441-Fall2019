56bcfeef7be3c14740cdd9745091ce0a7f581ec8
s_shehna <s_shehna@MARGARITA.ENCS.concordia.ca>
2019-11-07 14:18:38 -0500

    test suite for attack method

---
+++ RiskGameSOEN6441-Fall2019/src/test/TestSuite_Attack.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+/**
+ * This testsuite include all the testclasses related to Attack phase
+ * @author s_shehna
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ TestArmies.class, TestAttack.class, TestPlayer.class })
+public class TestSuite_Attack {
+
+}
