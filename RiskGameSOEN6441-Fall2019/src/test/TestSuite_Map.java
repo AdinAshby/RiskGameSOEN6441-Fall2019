e48e505016f561a0a89d71a939ede52daa5ce39f
s_shehna <s_shehna@MARGARITA.ENCS.concordia.ca>
2019-11-07 16:10:05 -0500

    test uite for maps

--- RiskGameSOEN6441-Fall2019/src/test/TestSuite_Map.java
+++ RiskGameSOEN6441-Fall2019/src/test/TestSuite_Map.java
@@ -3,9 +3,13 @@ package test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+/**
+ * This testsuite include all the testcase classes for map validation
+ * @author s_shehna
+ *
+ */
 @RunWith(Suite.class)
-@SuiteClasses({ TestMap.class, TestMapFiles.class, TestPlayer.class, TestReinforcement.class, TestArmies.class })
-public class TestSuite {
+@SuiteClasses({ TestMap.class, TestMapFiles.class})
+public class TestSuite_Map {
 
 }
