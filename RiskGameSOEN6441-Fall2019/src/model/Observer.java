b476e8e7e29d6ebc4f87db0296f03aaf4acf27f6
Golnoosh72 <yazdani7@yahoo.com>
2019-11-07 15:49:07 -0500

    comments

--- RiskGameSOEN6441-Fall2019/src/model/Observer.java
+++ RiskGameSOEN6441-Fall2019/src/model/Observer.java
@@ -1,7 +1,11 @@
 package model;
 
 import java.util.ArrayList;
-
+/**
+ * This is interface for the Observer
+ * @author f_yazdan
+ *
+ */
 public interface Observer {
 	public void update(int counterForPhases, String playerName);
 	public void update(double percentageControlled, int totalNumberOfArmies, ArrayList<String> continentsControlled);
