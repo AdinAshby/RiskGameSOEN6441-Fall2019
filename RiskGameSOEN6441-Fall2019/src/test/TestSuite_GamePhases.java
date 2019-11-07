5e88aa9d6204a6b840add84ffeca0c2832bda416
s_shehna <s_shehna@MARGARITA.ENCS.concordia.ca>
2019-11-07 14:17:14 -0500

    test suite for game phase validation

---
+++ RiskGameSOEN6441-Fall2019/src/test/TestSuite_GamePhases.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+/**
+ * This test suite include test classes for different game phases
+ * @author s_shehna
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ TestFortify.class, TestReinforcement.class })
+public class TestSuite_GamePhases {
+
+}
