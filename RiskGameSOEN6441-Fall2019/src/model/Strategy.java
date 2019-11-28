330fa413bb2306ad37784c88bf564b53c19b0767
Babita Bashal <b_bashal@encs.concordia.ca>
2019-11-27 20:47:36 -0800

    Strategy class updated

--- RiskGameSOEN6441-Fall2019/src/model/Strategy.java
+++ RiskGameSOEN6441-Fall2019/src/model/Strategy.java
@@ -1,4 +1,11 @@
 package model;
+/**
+ * This is Strategy interface for implementing strategy pattern
+ * @author f_yazdan
+ * @author s_shehna
+ * @author AdinAshby
+ * @author Babita kaur
+ */
 
 public interface Strategy {
 	public void attack(Country attackerCountry, Country attackingCountry, int attackerNumDice, int defendNumDice, int attackAllout);
